16aab53a51aa751c104ba1a4984768b144f47ff6
Stefan Reck <reck-stefan@web.de>
2017-06-08 22:30:43 +0200

    First JSF working!!

---
+++ src/main/java/beans/BookListBean.java
@@ -0,0 +1,15 @@
+package beans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ * Created by smint on 08.06.17.
+ */
+
+@ManagedBean
+@SessionScoped
+public class BookListBean {
+
+
+}
