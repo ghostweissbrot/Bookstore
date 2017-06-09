e6d7d6799df7329019b9fc611aca7483e30d66d5
Stefan Reck <reck-stefan@web.de>
2017-06-09 15:54:05 +0200

    Adding and removing to basket is working. Need to fix adding from category-site

---
+++ src/main/java/beans/ShoppingCartBean.java
@@ -0,0 +1,76 @@
+package beans;
+
+import backend.Book;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by smint on 09.06.17.
+ */
+
+@ManagedBean
+@SessionScoped
+
+public class ShoppingCartBean {
+
+    private List<Book> basket = new ArrayList<Book>();
+    private Map<Book, Integer> quantities = new HashMap<Book, Integer>();
+    private double summary = 0;
+
+    public List<Book> getBasket() {
+        return basket;
+    }
+
+    public double getSummary() {
+        summary *= 100;
+        int temp = (int) summary;
+        summary = (double) temp / 100.0;
+        return summary;
+    }
+
+    public String addBook(Book book) {
+        if(basket.contains(book)) {
+            quantities.put(book ,quantities.get(book) + 1);
+        }
+        else {
+            basket.add(book);
+            quantities.put(book, 1);
+        }
+        if(book != null) {
+            summary += book.getPrice();
+        }
+        return "/shoppingcart.xhtml";
+    }
+
+    public int getQuantity(Book book) {
+        return quantities.get(book);
+    }
+
+    public List<Integer> getAllowedQuantities() {
+        List<Integer> temp = new ArrayList<Integer>();
+        temp.add(1);
+        temp.add(2);
+        temp.add(3);
+        temp.add(4);
+        temp.add(5);
+        return temp;
+    }
+
+    public String removeBook(Book book) {
+        if(basket.contains(book)) {
+            if(quantities.get(book) == 1) {
+                basket.remove(book);
+                quantities.remove(book);
+            } else {
+                quantities.put(book ,quantities.get(book) - 1);
+            }
+            summary -= book.getPrice();
+        }
+        return "shoppingcart.xhtml";
+    }
+}
